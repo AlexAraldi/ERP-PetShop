6571a691323dbb875a90e329089086ae39e7fb40
AlexAraldi <alexanderaraldi@gmail.com>
2025-05-31 16:09:04 -0300

    style: Correção de bugs e reestruturação da POO. Construtor redirecionado e linhas alteradas.

--- PetShopConsoleApp1/Program.cs
+++ PetShopConsoleApp1/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace PetShopConsoleApp1
+﻿
+using PetShopConsoleApp1.Compartilhado;
+
+namespace PetShopConsoleApp1;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        TelaPrincipal telaPrincipal = new TelaPrincipal();
+
+        while (true)
         {
-            Console.WriteLine("Hello, World!");
+            telaPrincipal.ApresentarMenuPrincipal();
+
+            ITelaCrud telaSelecionada = telaPrincipal.ObterTela();
+
+            if (telaSelecionada == null)
+                break;
+
+            char opcaoEscolhida = telaSelecionada.ApresentarMenu();
+
+            switch (opcaoEscolhida)
+            {
+                case '1': telaSelecionada.CadastrarRegistro(); break;
+                case '2': telaSelecionada.EditarRegistro(); break;
+                case '3': telaSelecionada.ExcluirRegistro(); break;
+                case '4': telaSelecionada.VisualizarRegistros(true); break;
+
+                default: break;
+            }
         }
     }
 }
