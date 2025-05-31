926567ab152cfb581e148f37b4450fb8f50aa9fb
AlexAraldi <alexanderaraldi@gmail.com>
2025-05-31 16:02:11 -0300

    Implementado ModuloDono

---
+++ PetShopConsoleApp1/ModuloDono/Dono.cs
@@ -0,0 +1,33 @@
+﻿using PetShopConsoleApp1.Compartilhado;
+
+namespace PetShopConsoleApp1.ModuloDono;
+
+public class Dono : EntidadeBase<Dono>
+{
+    public Dono(string nome)
+    {
+        this.Nome = nome;
+    }
+
+    public string Nome { get; set; }
+
+    public override void AtualizarRegistro(Dono donoEditado)
+    {
+        Nome = donoEditado.Nome;
+    }
+
+    public override string Validar()
+    {
+        string erros = "";
+
+        if (string.IsNullOrEmpty(Nome))
+            erros += "O campo nome é obrigatório";
+
+        return erros;
+    }
+
+    public override string ToString()
+    {
+        return $"Id: {Id}, nome: {Nome}";
+    }
+}
---
+++ PetShopConsoleApp1/ModuloDono/TelaDono.cs
@@ -0,0 +1,32 @@
+﻿using PetShopConsoleApp1.Compartilhado;
+
+namespace PetShopConsoleApp1.ModuloDono;
+
+public class TelaDono : TelaBase<Dono>, ITelaCrud
+{
+    public TelaDono(string nomeEntidade, RepositorioDono repositorio) : base(nomeEntidade, repositorio)
+    {
+    }
+
+    protected override void ExibirCabecalhoTabela()
+    {
+        Console.WriteLine("{0, -10} | {1, -20}", "Id", "Nome");
+    }
+
+    protected override void ExibirLinhaTabela(Dono dono)
+    {
+        Console.WriteLine("{0, -10} | {1, -20}", dono.Id, dono.Nome);
+    }
+
+    protected override Dono ObterDados()
+    {
+        Console.Write("Digite o nome: ");
+        string nome = Console.ReadLine();
+
+        Dono novoDono = new Dono(nome);
+
+        return novoDono;
+    }
+
+
+}
